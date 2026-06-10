54d93e9d76f60acecf4c1a0cdb31f50d7f071ce8
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-09 23:23:38 -0400

    feat: update pom y config

--- Consulta Medica y Dental/ms-auth/ms-auth/src/main/java/com/example/ms_auth/config/SecurityConfig.java
+++ Consulta Medica y Dental/ms-auth/ms-auth/src/main/java/com/example/ms_auth/config/SecurityConfig.java
@@ -2,10 +2,9 @@ package com.example.ms_auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {

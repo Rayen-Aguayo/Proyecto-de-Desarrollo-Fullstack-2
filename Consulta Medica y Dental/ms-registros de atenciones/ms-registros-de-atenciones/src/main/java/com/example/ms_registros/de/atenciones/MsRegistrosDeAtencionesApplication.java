71d7b930f6397676366be66ce3524d1279789677
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-10 18:19:21 -0400

    Add files via upload

---
+++ Consulta Medica y Dental/ms-registros de atenciones/ms-registros-de-atenciones/src/main/java/com/example/ms_registros/de/atenciones/MsRegistrosDeAtencionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_registros.de.atenciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistrosDeAtencionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegistrosDeAtencionesApplication.class, args);
+	}
+
+}

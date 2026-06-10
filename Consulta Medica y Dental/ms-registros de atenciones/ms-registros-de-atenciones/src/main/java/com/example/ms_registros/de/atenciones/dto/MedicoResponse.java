7da0cdee0361781eafda734a92be07c5434be735
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-09 21:53:18 -0400

    feat: update registro de atenciones

---
+++ Consulta Medica y Dental/ms-registros de atenciones/ms-registros-de-atenciones/src/main/java/com/example/ms_registros/de/atenciones/dto/MedicoResponse.java
@@ -0,0 +1,12 @@
+package com.example.ms_registros.de.atenciones.dto;
+
+import lombok.Data;
+
+@Data
+
+public class MedicoResponse {
+    
+    private String runMedico;
+    private String nombreMedico;
+}
+
---
+++ Consulta Medica y Dental/ms-registros de atenciones/ms-registros-de-atenciones/src/main/java/com/example/ms_registros/de/atenciones/dto/PacienteResponse.java
@@ -0,0 +1,11 @@
+package com.example.ms_registros.de.atenciones.dto;
+
+import lombok.Data;
+
+@Data
+
+public class PacienteResponse {
+
+    private String runPaciente;
+    private String nombrePaciente;
+}

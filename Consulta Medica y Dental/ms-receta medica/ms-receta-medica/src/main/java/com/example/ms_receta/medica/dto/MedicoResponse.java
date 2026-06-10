55b6997c60c1bede8760c052dbcec39e6c287b92
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-09 21:36:46 -0400

    feat: update receta medica

---
+++ Consulta Medica y Dental/ms-receta medica/ms-receta-medica/src/main/java/com/example/ms_receta/medica/dto/MedicoResponse.java
@@ -0,0 +1,10 @@
+package com.example.ms_receta.medica.dto;
+
+import lombok.Data;
+
+@Data
+public class MedicoResponse {
+    private String nombreMedico;
+    private String runMedico;
+    private String firmaMedico;
+}

20006f472d6e8542a69b0bb3d58a4f730294e195
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-09 21:29:27 -0400

    feat: update  opinion pagos

---
+++ Consulta Medica y Dental/ms-opinion del paciente/ms-opinion-del-paciente/src/main/java/com/example/ms_opinion/del/paciente/repository/OpinionPacienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.ms_opinion.del.paciente.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Experiencia.del.Paciente.model.OpinionPaciente;
+
+public interface OpinionPacienteRepository extends JpaRepository<OpinionPaciente, Long>{
+
+}

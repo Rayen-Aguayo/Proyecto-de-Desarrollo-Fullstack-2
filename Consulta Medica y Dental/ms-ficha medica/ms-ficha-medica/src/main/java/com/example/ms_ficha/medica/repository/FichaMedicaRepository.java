0cb5da2d9c2bc159d9b404e0a0939111f806a87e
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-10 17:59:51 -0400

    Delete Consulta Medica y Dental directory

--- Consulta Medica y Dental/ms-ficha medica/ms-ficha-medica/src/main/java/com/example/ms_ficha/medica/repository/FichaMedicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Ficha.medica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Ficha.medica.model.FichaMedica;
-
-public interface FichaMedicaRepository extends JpaRepository<FichaMedica,Long> {
-
-}

af76cc87b04dd5cfe60301d3f980876dd8c747b8
javiera-faundez <ja.faundezb@duocuc.cl>
2026-06-09 20:28:55 -0400

    feat: update facturacion y presupuesto

--- Consulta Medica y Dental/ms-facturacion y presupuesto/ms-facturacion-y-presupuesto/src/main/java/com/example/ms_facturacion/y/presupuesto/client/PacienteClient.java
+++ Consulta Medica y Dental/ms-facturacion y presupuesto/ms-facturacion-y-presupuesto/src/main/java/com/example/ms_facturacion/y/presupuesto/client/PacienteClient.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.Facturacion.y.Presupuesto.dto.ApiResponse;
-import com.example.Facturacion.y.Presupuesto.dto.PacienteResponse;
+import com.example.ms_facturacion.y.presupuesto.dto.PacienteResponse;
 
 import lombok.RequiredArgsConstructor;
 

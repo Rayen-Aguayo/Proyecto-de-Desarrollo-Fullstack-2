14797aae452997d88b449bdb33e52320b31f1e91
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-10 23:34:30 -0400

    feat: update repository

--- Consulta Medica y Dental/ms-paciente/ms-paciente/src/main/java/com/example/ms_paciente/repository/PacienteRepository.java
+++ Consulta Medica y Dental/ms-paciente/ms-paciente/src/main/java/com/example/ms_paciente/repository/PacienteRepository.java
@@ -1,11 +1,8 @@
 package com.example.ms_paciente.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.ms_paciente.model.Paciente;
 
-@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, String> {
 
 }
